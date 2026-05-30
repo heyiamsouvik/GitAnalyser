b3d96c732f8a81250ecb2d9ae3555277003be697
heyiamsouvik <heyiamsouvik@gmail.com>
2026-05-30 13:35:07 +0530

    minor changes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = require("./src/app");
 const initializeDatabase = require("./src/config/initDb");
 
 
+const PORT = process.env.PORT || 5000;
+
 (async () => {
   await initializeDatabase();
 
